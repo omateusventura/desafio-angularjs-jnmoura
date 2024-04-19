78aa72a9ccf84d1ee29da687976c296b33f13a0f
mateus <mateus.ventura170@gmail.com>
2024-04-19 12:52:42 -0300

    add env

--- src/app/services/people.service.ts
+++ src/app/services/people.service.ts
@@ -1,39 +1,32 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
 
-  apiUrl = 'http://localhost:5252/api/people';
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  }
-
   constructor(
     private httpClient: HttpClient
   ) { }
 
   public getAll() {
-    return this.httpClient.get(this.apiUrl);
+    return this.httpClient.get(environment.apiUrl);
   }
 
   public findById(peopleId: number) {
-    return this.httpClient.get(`${this.apiUrl}/${peopleId}`);
+    return this.httpClient.get(`${environment.apiUrl}/${peopleId}`);
   }
 
   public add(people: any) {
-    return this.httpClient.post(this.apiUrl, people);
+    return this.httpClient.post(environment.apiUrl, people);
   }
 
   public update(people: any, peopleId: number) {
-    return this.httpClient.put(`${this.apiUrl}/${peopleId}`, people);
+    return this.httpClient.put(`${environment.apiUrl}/${peopleId}`, people);
   }
   public delete(peopleId: number) {
-    return this.httpClient.delete(`${this.apiUrl}/${peopleId}`);
+    return this.httpClient.delete(`${environment.apiUrl}/${peopleId}`);
   }
 }
